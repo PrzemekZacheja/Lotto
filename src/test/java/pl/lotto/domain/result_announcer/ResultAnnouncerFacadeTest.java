f5102170c65aab12f5b4f82c5a16b606d08c41da
PZacheja <przemek.zacheja@gmail.com>
2023-03-31 11:04:14 +0200

    added test packages

---
+++ src/test/java/pl/lotto/domain/result_announcer/ResultAnnouncerFacadeTest.java
@@ -0,0 +1,7 @@
+package pl.lotto.domain.result_announcer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultAnnouncerFacadeTest {
+
+}
---
+++ src/test/java/pl/lotto/domain/result_checker/ResultCheckerFacadeTest.java
@@ -0,0 +1,7 @@
+package pl.lotto.domain.result_checker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultCheckerFacadeTest {
+
+}
