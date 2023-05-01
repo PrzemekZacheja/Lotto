3f92d803943b9295d4c4a05cb6ebbbb2cb6a0a22
PZacheja <przemek.zacheja@gmail.com>
2023-05-01 20:54:43 +0200

    implement one number from http client taker.

--- src/main/java/pl/lotto/domain/numbers_generator/WinningNumberGenerable.java
+++ src/main/java/pl/lotto/domain/numbers_generator/WinningNumberGenerable.java
@@ -2,7 +2,7 @@ package pl.lotto.domain.numbers_generator;
 
 import java.util.Set;
 
-public interface WinningNumberGenerable {
+public interface WinningNumberGenerable extends OneNumberGeneratorFetcher {
 
     Set<Integer> generateWinningRandomNumbers();
 }
