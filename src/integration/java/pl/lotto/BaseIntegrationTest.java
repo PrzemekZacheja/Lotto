729f71e9008b912b33b3dc3fa3d477fd0ebb87f4
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-05-20 21:50:25 +0200

    prepare BaseIntegrationTest class and happypath with dependecies

--- src/integration/java/pl/lotto/BaseIntegrationTest.java
+++ src/integration/java/pl/lotto/BaseIntegrationTest.java
@@ -1,16 +1,52 @@
 package pl.lotto;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.utility.DockerImageName;
 import pl.lotto.domain.LottoSpringBootApplication;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = LottoSpringBootApplication.class)
-class BaseIntegrationTest {
+@ActiveProfiles("integration")
+@AutoConfigureMockMvc
+@Testcontainers
+public class BaseIntegrationTest {
+
+    @Container
+    public static final MongoDBContainer mvcContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+    private static final String WIRE_MOCK_HOST = "http://localhost";
+    @RegisterExtension
+    public static WireMockExtension wireMockExtension = WireMockExtension.newInstance()
+            .options(wireMockConfig().dynamicPort())
+            .build();
+    @Autowired
+    public MockMvc mvc;
+    @Autowired
+    public ObjectMapper objectMapper;
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mvcContainer::getReplicaSetUrl);
+        registry.add("offer.http.client.config.uri", () -> WIRE_MOCK_HOST);
+        registry.add("offer.http.client.config.port", () -> wireMockExtension.getPort());
+    }
 
     @Test
-    void shouldTest(){
+    void shouldTest() {
         assertTrue(true);
     }
 }
