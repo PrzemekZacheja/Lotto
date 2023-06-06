ad2a2cb6415b933c9d420f8a03be18b9ea7ac25d
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-06-06 21:26:31 +0200

    added NumbersGeneratorRestTemplateConfigurationProperties

--- src/main/java/pl/lotto/infrastucture/numbersgenerator/http/NumbersGeneratorRestTemplateConfigurationProperties.java
+++ src/main/java/pl/lotto/infrastucture/numbersgenerator/http/NumbersGeneratorRestTemplateConfigurationProperties.java
@@ -8,6 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public record NumbersGeneratorRestTemplateConfigurationProperties(
         String uri,
         int port,
-        long timeout,
+        long connectionTimeout,
         long readTimeout) {
 }
