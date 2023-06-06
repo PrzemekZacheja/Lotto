fe22a78fc8db31f4c546ee51ca34197ffb2dfefd
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-06-06 21:25:24 +0200

    added NumbersGeneratorRestTemplateConfigurationProperties

--- src/main/java/pl/lotto/LottoSpringBootApplication.java
+++ src/main/java/pl/lotto/LottoSpringBootApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import pl.lotto.domain.numbersgenerator.NumbersGeneratorFacadeConfigurationProperties;
+import pl.lotto.infrastucture.numbersgenerator.http.NumbersGeneratorRestTemplateConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({NumbersGeneratorFacadeConfigurationProperties.class})
+@EnableConfigurationProperties({NumbersGeneratorFacadeConfigurationProperties.class, NumbersGeneratorRestTemplateConfigurationProperties.class})
 public class LottoSpringBootApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/pl/lotto/infrastucture/numbersgenerator/http/NumbersGeneratorRestTemplateConfigurationProperties.java
@@ -0,0 +1,13 @@
+package pl.lotto.infrastucture.numbersgenerator.http;
+
+import lombok.Builder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "lotto.number-generator.http.client.config")
+@Builder
+public record NumbersGeneratorRestTemplateConfigurationProperties(
+        String uri,
+        int port,
+        long timeout,
+        long readTimeout) {
+}
