63ac9ea9790082fd5caf85f1ff5d0544cb2705e1
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-09-23 15:30:07 +0200

    Upgrade versions in pom.xml

--- src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
+++ src/integration/java/pl/lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -27,7 +27,12 @@ class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
     void should_user_win_and_system_should_generate_winner() throws Exception {
 //        step 1: external service returns 6 random numbers (1,2,3,4,5,6)
         //given
-        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25").willReturn(WireMock.aResponse().withStatus(HttpStatus.OK.value()).withHeader("Content-Type", "application/json").withBody("[43, 5, 80, 14, 62, 31, 99, 51, 22, 40, 4, 75, 71, 31, 30, 66, 64, 53, 78, 72, 68, 4, 62, 70, 84]")));
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                                       .willReturn(WireMock.aResponse()
+                                                           .withStatus(HttpStatus.OK.value())
+                                                           .withHeader("Content-Type", "application/json")
+                                                           .withBody(
+                                                                   "[43, 5, 80, 14, 62, 31, 99, 51, 22, 40, 4, 75, 71, 31, 30, 66, 64, 53, 78, 72, 68, 4, 62, 70, 84]")));
         //when
         numbersGeneratorFacade.generateSixNumbers();
 
@@ -36,14 +41,18 @@ class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
         //given
         LocalDateTime drawDate = LocalDateTime.of(2022, 11, 26, 12, 0, 0);
         //when & then
-        await().atMost(Duration.ofSeconds(20)).pollInterval(Duration.ofSeconds(1)).until(() -> {
-            try {
-                return !numbersGeneratorFacade.retrieveAllWinnerNumbersByNextDrawDate(drawDate).winningNumbers().isEmpty();
+        await().atMost(Duration.ofSeconds(20))
+               .pollInterval(Duration.ofSeconds(1))
+               .until(() -> {
+                   try {
+                       return !numbersGeneratorFacade.retrieveAllWinnerNumbersByNextDrawDate(drawDate)
+                                                     .winningNumbers()
+                                                     .isEmpty();
 
-            } catch (WinningNunmbersNotFoundExeption exception) {
-                return false;
-            }
-        });
+                   } catch (WinningNunmbersNotFoundExeption exception) {
+                       return false;
+                   }
+               });
 
 
 //        step 3: user made POST /inputNumbers with 6 numbers (1, 2, 3, 4, 5, 6) at 16-11-2022 10:00 and system returned OK(200) with message:
@@ -51,20 +60,23 @@ class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
         //given
         //when
         ResultActions perform = mockMvc.perform(post("/inputNumbers").content("""
-                {
-                "inputNumbers" : [1, 2, 3, 4, 5, 6]
-                }
-                """.trim()).contentType(MediaType.APPLICATION_JSON));
+                                                                              {
+                                                                              "inputNumbers" : [1, 2, 3, 4, 5, 6]
+                                                                              }
+                                                                              """.trim())
+                                                                     .contentType(MediaType.APPLICATION_JSON));
         //then
-        MvcResult mvcResult = perform.andExpect(status().isOk()).andReturn();
-        String contentAsString = mvcResult.getResponse().getContentAsString();
+        MvcResult mvcResult = perform.andExpect(status().isOk())
+                                     .andReturn();
+        String contentAsString = mvcResult.getResponse()
+                                          .getContentAsString();
         TicketDto ticketDto = objectMapper.readValue(contentAsString, TicketDto.class);
 
         assertAll(
                 () -> assertThat(ticketDto.drawDate()).isEqualTo(drawDate),
                 () -> assertThat(ticketDto.ticketId()).isNotNull(),
                 () -> assertThat(ticketDto.message()).isEqualTo("Success")
-        );
+                 );
 
 
 //        step 4: user made GET /results/notExistingId and system returned 404(NOT_FOUND) and body with (“message”: “Not found for id: notExistingId” and “status”: “NOT_FOUND”)
