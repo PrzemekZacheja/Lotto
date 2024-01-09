2831f40a652a3b34a708a9ddda0c3aacb155ebb8
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-01-09 14:18:51 +0100

    added unit tests for LoginAndRegisterFacade

---
+++ src/test/java/pl/lotto/domain/loginandregister/LoginAndRegisterFacadeRepositoryForTest.java
@@ -0,0 +1,139 @@
+package pl.lotto.domain.loginandregister;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+public class LoginAndRegisterFacadeRepositoryForTest implements LoginAndRegisterFacadeRepository {
+
+    Map<String, User> databaseInMemory = new ConcurrentHashMap<>();
+
+    @Override
+    public User save(User user) {
+        databaseInMemory.put(user.getEmail(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String emailToSearchInDatabase) {
+        return Optional.ofNullable(databaseInMemory.get(emailToSearchInDatabase));
+    }
+
+    @Override
+    public <S extends User> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public Iterable<User> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends User> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends User, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
+    }
+}
---
+++ src/test/java/pl/lotto/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,60 @@
+package pl.lotto.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.BadCredentialsException;
+import pl.lotto.domain.loginandregister.dto.UserDto;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class LoginAndRegisterFacadeTest {
+
+    LoginAndRegisterFacadeRepository repository = new LoginAndRegisterFacadeRepositoryForTest();
+    LoginAndRegisterFacade loginAndRegisterFacade = new LoginAndRegisterFacade(repository);
+
+    @Test
+    void should_save_user_to_database_if_not_exist() {
+        //given
+        String email = "example@gov.pl";
+        String password = "1234";
+        //when
+        UserDto registeredUser = loginAndRegisterFacade.registerUser(email, password);
+        //then
+        Optional<User> repositoryByEmail = repository.findByEmail(email);
+        assertThat(registeredUser).isEqualTo(MapperLoginAndRegister.mapToUserDto(repositoryByEmail.get()));
+    }
+
+    @Test
+    void should_create_User_and_set_isLogged_to_false_when_user_not_exist_in_repository() {
+        //given
+        String email = "example@gov.pl";
+        String password = "1234";
+        //when
+        UserDto firstLogin = loginAndRegisterFacade.registerUser(email, password);
+        //then
+        assertThat(firstLogin.isLogged()).isFalse();
+    }
+
+
+    @Test
+    void should_change_isLogged_to_true_when_user_exist_in_repository() {
+        //given
+        String email = "example@gov.pl";
+        String password = "1234";
+        //when
+        UserDto firstLogin = loginAndRegisterFacade.registerUser(email, password);
+        UserDto secondLogin = loginAndRegisterFacade.registerUser(email, password);
+        //then
+        assertThat(secondLogin.isLogged()).isTrue();
+    }
+
+    @Test
+    void should_throw_exception_when_user_is_not_found() {
+        //given
+        String email = "example@gov.pl";
+        //when
+        //then
+        assertThatThrownBy(() -> loginAndRegisterFacade.loginUser(email)).isInstanceOf(BadCredentialsException.class);
+    }
+}
