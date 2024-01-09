61a952a2c6730a161cbf77e240a645cd7956a67b
Przemysław Zacheja <przemek.zacheja@gmail.com>
2024-01-09 14:19:25 +0100

    created UserSecurityIntegrationTest for integration tests

---
+++ src/integration/java/pl/lotto/security/UserSecurityIntegrationTest.java
@@ -0,0 +1,14 @@
+package pl.lotto.security;
+
+import org.junit.jupiter.api.Test;
+import pl.lotto.BaseIntegrationTest;
+
+public class UserSecurityIntegrationTest extends BaseIntegrationTest {
+
+
+    @Test
+    void should_register_user_and_login() {
+
+        //1. User próbuje uzyskać JWT Token przez POST /token with username
+    }
+}
